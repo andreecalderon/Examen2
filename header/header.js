26b7b1beed46668fd8ab8319f71b7e73a341a12f
andreecalderon <jacalderon@scl.edu.gt>
2024-07-11 12:25:15 -0600

    se arregla el tamaño del header

--- header/header.js
+++ header/header.js
@@ -3,7 +3,7 @@ DOM_header.innerHTML = `
 
 <div class="contenedor">
     <div class="cuadro">
-        <h1>jacalderon@scl.edu.gt</h1>
+        <h3>jacalderon@scl.edu.gt</h3>
         <a href="https://github.com/andreecalderon/Examen2" class="boton">github</a>
     </div>
 </div>
